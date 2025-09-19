d9b39be781f9d9053641a885d3269b2d15f90942
Rebbalapalli Pranav Maruthi <pranavrebbalapalli@gmail.com>
2025-09-19 15:11:57 +0530

    Delete backend/dist directory

--- backend/dist/route/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { registerUser, loginUser, getUserProfile, logoutUser } from "../controllers/authController.js";
-const router = express.Router();
-router.post("/register", registerUser);
-router.post("/login", loginUser);
-router.get("/me", getUserProfile);
-router.post("/logout", logoutUser);
-export default router;
-//# sourceMappingURL=authRoutes.js.map
